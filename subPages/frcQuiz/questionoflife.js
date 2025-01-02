5295bc41a1ee04cd1ff439042e6aec6efad4d13b
SpaceStudios <109873883+SpaceStudios@users.noreply.github.com>
2025-01-02 15:40:40 +0000

    testing

--- subPages/frcQuiz/questionoflife.js
+++ subPages/frcQuiz/questionoflife.js
@@ -100,6 +100,7 @@ function loadQuestion() {
     document.getElementById("option3").addEventListener("click", selectOption3, false);
     document.getElementById("option4").addEventListener("click", selectOption4, false);
     document.getElementById("submit").addEventListener("click", submit, false);
+    console.log("test")
     console.log(parseInt(QuizSettings.CurrentQuestion) < parseInt(QuizSettings.QuizQuestions));
     if (parseInt(QuizSettings.CurrentQuestion) < parseInt(QuizSettings.QuizQuestions)) {
         document.getElementById("submit").setAttribute("href", "question.html");
